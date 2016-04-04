8d52ac7e31efbb2832b1c0b94eb53906f9d34af6
brogowski <bartlomiej.rogowski@gmail.com>
2016-04-04 19:33:30 +0200

    Completed BSP algorythm.

--- src/rl_engine/dungeon_generator/bsp/tree.clj
+++ src/rl_engine/dungeon_generator/bsp/tree.clj
@@ -1,5 +1,7 @@
 (ns rl-engine.dungeon-generator.bsp.tree)
 
+;TODO: change to recur
+
 (def HEIGHT "HEIGHT")
 (def WIDTH "WIDTH")
 (def MINIMUM_SIZE 3)
@@ -15,8 +17,16 @@
 (defn split-dimension-for-leaf-b
   [size ratio]
   (if (= 0 (mod size 2))
-         (int (* (- size 1) (- 1 ratio)))
-         (int (* size (- 1 ratio)))))
+    (int (* (- size 1) (- 1 ratio)))
+    (int (* size (- 1 ratio)))))
+
+(defn split-coordinate-for-leaf-a
+  [min-position max-position ratio]
+  min-position)
+
+(defn split-coordinate-for-leaf-b
+  [min-position max-position ratio]
+  (+ min-position (int (* max-position ratio))))
 
 (defn can-split-dimension?
   [size ratio]
@@ -31,45 +41,74 @@
     (can-split-dimension? height ratio)))
 
 (defn get-split-dimension
-  [height width ratio]
-  (if (can-split-dimension? height ratio)
-    HEIGHT
-    WIDTH))
+  [height width ratio randomizer]
+  (if (and
+        (can-split-dimension? height ratio)
+        (can-split-dimension? width ratio))
+    (if (>= (randomizer) 0.5)
+      WIDTH
+      HEIGHT)
+    (if (can-split-dimension? height ratio)
+      HEIGHT
+      WIDTH)))
+
+(defn get-leaf
+  [root split-dimension ratio split-dimension-func split-coordinate-func]
+  (let [height (:height root)
+        width (:width root)
+        top (:top root)
+        left (:left root)]
+    {:height (if (= HEIGHT split-dimension)
+               (+ 1 (split-dimension-func height ratio))
+               height)
+     :width  (if (= WIDTH split-dimension)
+               (+ 1 (split-dimension-func width ratio))
+               width)
+     :top    (if (= HEIGHT split-dimension)
+               (split-coordinate-func top height ratio)
+               top)
+     :left   (if (= WIDTH split-dimension)
+               (split-coordinate-func left width ratio)
+               left)}))
+
+(defn get-leaf-a
+  [root split-dimension ratio]
+  (get-leaf root split-dimension ratio
+            split-dimension-for-leaf-a
+            split-coordinate-for-leaf-a))
+
+(defn get-leaf-b
+  [root split-dimension ratio]
+  (get-leaf root split-dimension ratio
+            split-dimension-for-leaf-b
+            split-coordinate-for-leaf-b))
 
 (defn split-room
-  [height width ratio]
-  (let [split-dimension (get-split-dimension height width ratio)]
-    {:height height
-     :width  width
-     :left   0
-     :top    0
-     :leaf-a {:height (if (= HEIGHT split-dimension)
-                        (+ 1 (split-dimension-for-leaf-a height ratio))
-                        height)
-              :width  (if (= WIDTH split-dimension)
-                        (+ 1 (split-dimension-for-leaf-a width ratio))
-                        width)
-              :left   0
-              :top    0}
-     :leaf-b {:height (if (= HEIGHT split-dimension)
-                        (+ 1 (split-dimension-for-leaf-b height ratio))
-                        height)
-              :width  (if (= WIDTH split-dimension)
-                        (+ 1 (split-dimension-for-leaf-b width ratio))
-                        width)
-              :left   0
-              :top    0}}))
+  [root randomizer]
+  (let [height (:height root)
+        width (:width root)
+        top (:top root)
+        left (:left root)
+        ratio (randomizer)]
+    (if (can-split-room? height width ratio)
+      (let [split-dimension (get-split-dimension height width ratio randomizer)]
+        {:height height
+         :width  width
+         :top    top
+         :left   left
+         :leaf-a (split-room (get-leaf-a root split-dimension ratio) randomizer)
+         :leaf-b (split-room (get-leaf-b root split-dimension ratio) randomizer)})
+      root)))
 
 (defn generate-rooms-tree
   "Generates tree of rooms."
   ([height width]
    (generate-rooms-tree height width (constantly (rand))))
   ([height width randomizer]
-   (when (is-space-for-room? height width)
-     (let [ratio (randomizer)]
-       (if (can-split-room? height width ratio)
-         (split-room height width ratio)
-         {:height height
-          :width  width
-          :left   0
-          :top    0})))))
+   (if (is-space-for-room? height width)
+     (let [root {:height height
+                 :width  width
+                 :left   0
+                 :top    0}]
+       (split-room root randomizer))
+     {})))
--- test/rl_engine/dungeon_generator/bsp/tree_test.clj
+++ test/rl_engine/dungeon_generator/bsp/tree_test.clj
@@ -3,14 +3,16 @@
             [rl-engine.dungeon-generator.test-utils :refer :all]
             [rl-engine.dungeon-generator.bsp.tree :refer :all]))
 
-;TODO: change utils to recur
+;TODO: change to recur
 
 (defn rooms-count
   [rooms-tree]
   (if (nil? rooms-tree)
     0
     (if (and (nil? (:leaf-a rooms-tree)) (nil? (:leaf-b rooms-tree)))
-      1
+      (if (= {} rooms-tree)
+        0
+        1)
       (+ (rooms-count (:leaf-a rooms-tree))
          (rooms-count (:leaf-b rooms-tree))))))
 
@@ -19,9 +21,21 @@
   (if (nil? rooms-tree)
     []
     (if (and (nil? (:leaf-a rooms-tree)) (nil? (:leaf-b rooms-tree)))
-      [(:height rooms-tree), (:width rooms-tree)]
-      [(flatten-rooms (:leaf-a rooms-tree)),
-       (flatten-rooms (:leaf-b rooms-tree))])))
+      rooms-tree
+      (flatten [(flatten-rooms (:leaf-a rooms-tree)),
+                (flatten-rooms (:leaf-b rooms-tree))]))))
+
+(defn flatten-rooms-dimensions
+  [rooms-tree]
+  (map
+    #((constantly [(:height %), (:width %)]))
+    (flatten-rooms rooms-tree)))
+
+(defn flatten-rooms-coordinates
+  [rooms-tree]
+  (map
+    #((constantly [(:top %), (:left %)]))
+    (flatten-rooms rooms-tree)))
 
 (deftest generates-empty-tree
   (testing "when size is invalid returns empty tree"
@@ -83,11 +97,11 @@
           ;1 1 1
           ;1 0 1
           ;1 1 1
-          (is (= [[3, 3], [3, 3]] (flatten-rooms (generate-rooms-tree 3 5 randomizer))))
+          (is (= [[3, 3], [3, 3]] (flatten-rooms-dimensions (generate-rooms-tree 3 5 randomizer))))
           ;1 1 1 1 1
           ;1 0 1 0 1
           ;1 1 1 1 1
-          (is (= [[3, 3], [3, 3]] (flatten-rooms (generate-rooms-tree 5 3 randomizer)))))
+          (is (= [[3, 3], [3, 3]] (flatten-rooms-dimensions (generate-rooms-tree 5 3 randomizer)))))
         (testing "even dimension"
           ; 1 1 1
           ; 1 0 1
@@ -95,10 +109,69 @@
           ; 1 1 1
           ; 1 0 1
           ; 1 1 1
-          (is (= [[4, 3], [3, 3]] (flatten-rooms (generate-rooms-tree 6 3 randomizer))))
+          (is (= [[4, 3], [3, 3]] (flatten-rooms-dimensions (generate-rooms-tree 6 3 randomizer))))
           ;1 1 1 1 1 1
           ;1 0 0 1 0 1
           ;1 1 1 1 1 1
-          (is (= [[3, 4], [3, 3]] (flatten-rooms (generate-rooms-tree 3 6 randomizer))))))
+          (is (= [[3, 4], [3, 3]] (flatten-rooms-dimensions (generate-rooms-tree 3 6 randomizer))))))
       (testing "coordinates are correct"
-        (is false)))))
+        (testing "odd dimension"
+          ;1 1 1
+          ;1 0 1
+          ;1 1 1
+          ;1 0 1
+          ;1 1 1
+          (is (= [[0, 0], [2, 0]] (flatten-rooms-coordinates (generate-rooms-tree 5 3 randomizer))))
+          ;1 1 1 1 1
+          ;1 0 1 0 1
+          ;1 1 1 1 1
+          (is (= [[0, 0], [0, 2]] (flatten-rooms-coordinates (generate-rooms-tree 3 5 randomizer))))
+          )
+        (testing "even dimension"
+          ; 1 1 1
+          ; 1 0 1
+          ; 1 0 1
+          ; 1 1 1
+          ; 1 0 1
+          ; 1 1 1
+          (is (= [[0, 0], [3, 0]] (flatten-rooms-coordinates (generate-rooms-tree 6 3 randomizer))))
+          ;1 1 1 1 1 1
+          ;1 0 0 1 0 1
+          ;1 1 1 1 1 1
+          (is (= [[0, 0], [0, 3]] (flatten-rooms-coordinates (generate-rooms-tree 3 6 randomizer)))))))))
+
+(deftest generates-recursive-rooms
+  (testing "when space allows for four rooms"
+    (testing "when randomizer allows for room split"
+      (let [randomizer (constantly 0.5)
+            rooms-tree (generate-rooms-tree 6 6 randomizer)]
+        ; 1 1 1 1 1 1
+        ; 1 0 0 1 0 1
+        ; 1 0 0 1 0 1
+        ; 1 1 1 1 1 1
+        ; 1 0 0 1 0 1
+        ; 1 1 1 1 1 1
+        (testing "room count is equal to 4"
+          (is (= 4 (rooms-count rooms-tree))))
+        (testing "dimensions are correct"
+          (is (= [[4, 4], [3, 4], [4, 3], [3, 3]] (flatten-rooms-dimensions rooms-tree))))
+        (testing "coordinates are correct"
+          (is (= [[0, 0], [3, 0], [0, 3], [3, 3]] (flatten-rooms-coordinates rooms-tree))))))))
+
+(deftest split-collisions
+  (testing "when split can be done on both dimensions - randomizer decides"
+    ; 1 1 1 1 1
+    ; 1 0 0 0 1
+    ; 1 0 0 0 1
+    ; 1 0 0 0 1
+    ; 1 1 1 1 1
+    (let [height 5
+          width 5
+          first-two-rooms-dimensions #(constantly [[(:height (:leaf-a %)), (:width (:leaf-a %))],
+                                                   [(:height (:leaf-b %)), (:width (:leaf-b %))]])]
+      (testing "when randomizer returns 0.5 or greater - split is done verticaly"
+        (let [tree (generate-rooms-tree height width (constantly 0.5))]
+          (is (= [[5, 3], [5, 3]] ((first-two-rooms-dimensions tree))))))
+      (testing "when randomizer returns less then 0.5 - split is done horizontally"
+        (let [tree (generate-rooms-tree height width (constantly 0.49))]
+          (is (= [[3, 5], [3, 5]] ((first-two-rooms-dimensions tree)))))))))
